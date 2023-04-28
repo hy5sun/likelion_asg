67ebd07790780db4a9eac9200934ebef504bcf0b
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-04-28 23:56:20 +0900

    feat: create createUser, verifyEmail, login, userInfo dto

---
+++ week5/src/app.controller.ts
@@ -0,0 +1,77 @@
+import { Request } from 'express';
+import { Controller, Get, Req, Res, HttpCode, HttpException, Body, Param, Patch, BadRequestException, Header, Redirect, Query, Delete } from '@nestjs/common';
+import { AppService } from './app.service';
+
+
+@Controller()
+export class AppController {
+  usersService: any;
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/hello') //http://localhost:3000/hello로 접속 가능 | he*lo 라고 쓰면 가운데 어떤 문자가 와도 상관없이 라우팅 패스를 구성하겠다는 뜻.
+  getHello(@Req() req: Request): string { // req: 요청객체. HTTP 요청
+    return this.appService.getHello();
+  }
+
+  @Get()
+  findAll(@Res() res) {
+    const users = this.usersService.findAll()
+
+    return res.status(200).send(users);
+  }
+
+  // 상태코드 바꾸기 
+  @HttpCode(202)
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  // 예외처리
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    if (+id < 1) {
+      throw new BadRequestException('id는 0보다 큰 값이어야 합니다.');
+    }
+    return this.usersService.findOne(+id);
+  }
+
+  // 응답에 커스텀 헤더 추가: 인수로 헤더 이름과 값을 받음.
+  @Header('Custom', 'Test Header')
+  @Get(':id')
+  findOneWithHeader(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  // 리디렉션
+  @Redirect('https://nestjs.com', 301)
+  @Get(':id')
+  findOne1(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  // // 쿼리 매개변수로 버전 숫자를 전달받아 해당 버전의 페이지로 이동 예제
+  // @Get('redirect/docs')
+  // @Redirect('https://docs.nestjs.com', 302)
+  // getDocs(@Query('version') version) {
+  //   if (version && version === '5') {
+  //     return {url: 'https://docs.nestjs.com/v5/'}
+  //   }
+  // }
+
+  // 라우트 매개변수 : 경로를 구성하는 매개변수
+  // 1. 매개변수가 여러 개 전달될 경우 객체로 한 번에 받기. (But params 타입이 any가 되어 권장X)
+  @Delete(':userId/memo/:memoId')
+  deleteUserMemo(@Param() params: {[key: string]: string}) {
+    return 'userId: ${params.userId}, memoId: ${params.memoId}';
+  }
+
+  // 일반적인 방법: 라우팅 매개변수를 따로 받음
+  @Delete(':userId/memo/:memoId')
+  deleteUserMemo2(
+    @Param('userId') userId: string,
+    @Param('memoId') memoId: string,
+    ) {
+      return 'userId: ${userId}, memoId: ${memoId}';
+  }
+}
---
+++ week5/src/users/users.controller.ts
@@ -0,0 +1,52 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { Verify } from 'crypto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() dto: CreateUserDto): Promise<void> {
+    console.log(dto);
+  }
+
+  @Post('/email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<void> {
+    console.log(dto);
+  }
+
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    console.log(dto);
+    return;
+  }
+
+  @Get('/:id')
+  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
+    console.log(userId);
+    return;
+  }
+
+  // @Get()
+  // findAll() {
+  //   return this.usersService.findAll();
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.usersService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.usersService.update(+id, updateUserDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.usersService.remove(+id);
+  // }
+}
