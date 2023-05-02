876a41b425f01dae4bf8e9994c2d54d475b12c0d
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-05-03 00:31:58 +0900

    module 수정

--- week4/practice/src/posts/posts.module.ts
+++ week4/practice/src/posts/posts.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
-import { UsersService } from 'src/users/users.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  exports: [],
+  imports: [UsersModule],
   controllers: [PostsController],
-  providers: [PostsService, UsersService]
+  providers: [PostsService]
 })
 export class PostsModule {}
--- week4/practice/src/users/users.module.ts
+++ week4/practice/src/users/users.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PostsModule } from 'src/posts/posts.module';
 
 @Module({
-  imports: [PostsModule],
+  exports: [UsersService],
   controllers: [UsersController],
   providers: [UsersService]
 })
