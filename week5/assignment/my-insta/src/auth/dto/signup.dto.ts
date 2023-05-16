b2cb3b9043d5d0e656baf7c1b28a4e84164764b0
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-05-17 01:48:04 +0900

    add signupDto

---
+++ week5/assignment/my-insta/src/auth/dto/signup.dto.ts
@@ -0,0 +1,28 @@
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class SignupDto {
+  @IsEmail()
+  @IsString()
+  @MaxLength(60)
+  email: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(10)
+  name: string;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(30)
+  userId: string;
+
+  @IsString()
+  @Matches(/^[A-Za-z\d!@#$%^7*()]{8, 30}$/)
+  password: string;
+}
