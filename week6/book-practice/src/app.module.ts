77e88ed18e3fbf30175f735f920b36a2eb2c2e6d
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-05-20 10:54:42 +0900

    ch.11 winston 로거 적용

--- week6/book-practice/src/app.module.ts
+++ week6/book-practice/src/app.module.ts
@@ -2,9 +2,29 @@ import { Module } from '@nestjs/common';
 import { LoggerModule } from './logging/logger.module';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import * as winston from 'winston';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
 
 @Module({
-  imports: [LoggerModule],
+  imports: [
+    LoggerModule,
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          level: process.env.NODE_ENV === 'producton' ? 'info' : 'silly',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            nestWinstonModuleUtilities.format.nestLike('MyApp', {
+              prettyPrint: true,
+            }),
+          ),
+        }),
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
